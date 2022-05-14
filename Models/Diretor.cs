32ad9f9783773ccb46b76413276cc55856bb6d76
kelvinangelus <kelvinangelus@gmail.com>
2022-05-14 14:05:26 -0300

    Classes seladas

---
+++ Models/Diretor.cs
@@ -0,0 +1,12 @@
+namespace POO.Models
+{
+    public class Diretor : Professor
+    {
+        public override void Apresentar()
+        {
+            {
+                Console.WriteLine("Diretor");
+            }
+        }
+    }
+}
